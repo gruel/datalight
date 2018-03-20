d5ead87b938f484ad62108869c480975b8e67189
Nicolas Gruel <nicolas.gruel@manchester.ac.uk>
2018-03-20 20:33:56 +0000

    rebuilt docs

--- README.rst
+++
@@ -1,6 +0,0 @@
-#########
-Zenolight
-#########
-
-A small python software to upload lightform data on data repository like
-zenodo.
--- datalight/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-__author__ = """Nicolas Gruel"""
-__email__ = 'nicolas.gruel@manchester.ac.uk'
-__version__ = '0.1.0'
